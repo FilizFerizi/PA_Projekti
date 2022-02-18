22787d716a88f154be0490535bfff7b6c08ad8ee
smargdadaija <smaragdadaija@gmail.com>
2022-02-18 13:29:28 +0100

    rregullimet

--- controllers/Image_Controller.py
+++ controllers/Image_Controller.py
@@ -7,82 +7,65 @@ from pathlib import Path
 from time import sleep,ctime
 import time
 from turtle import width
+from unicodedata import name
 from PIL import Image
 from urllib import response
 from numpy import where
 import requests
+from scipy.__config__ import show
 from tabulate import tabulate
 from functools import partial
 from multiprocessing.dummy import Pool, Process
 from ast import arg
 
 
-def download_multi(dics, path):
-    print(len(dics))
+def download_multi(url,name, path):
+         
+    response = requests.get(url, stream=True)
+    file_name = url.split('/')[-1]
+    filename=name
+    file = open(path+filename+".jpg", "wb")
+    file.write(response.content)
+    file.close()
+    print(file_name, "DONE!" +" MULTIPROCCESING")
     
-    for p_id, p_info in dics.items():
+def thumbnail(url,name,resize,resizeName, path):
 
-        
-        # print ("filloj metodat")
-        # print("dics id", p_id)
-        # print("prinotohet emri i fotos")
-        # print(dics[p_id]['name'])
-        # for key in p_info:
-        #     print(key +":",p_info[key])
-
-        response = requests.get(dics[p_id]['url'], stream=True)
-        file_name = dics[p_id]['url'].split('/')[-1]
-        filename=dics[p_id]['name']
+        response = requests.get(url, stream=True)
+        file_name = url.split('/')[-1]
+        filename=name
         file = open(path+filename+".jpg", "wb")
         
         file.write(response.content)
         file.close()
-
-
-        # image = Image.open(path+filename+'.jpg')
-        # # # image = image.convert('L')
-        # image.save(path+filename+".jpg")
-        print(file_name, "DONE!" +" MULTIPROCCESING")
-    
-        # thumbnail()
-
-
-
-
-
-
-
-
-def thumbnail(dics, path):
-  for p_id, p_info in dics.items():
-        response = requests.get(dics[p_id]['url'])
-        file_name = dics[p_id]['url'].split('/')[-1]
-        filename=dics[p_id]['name']
-        file = open(path+file_name, "wb")
-        file.write(response.content)
-        file.close()
         # print(file)
-
-        
-        
-            
-        image = Image.open(path+file_name)
+        image = Image.open(path+filename+".jpg")
         
         image.show()
-        width=int(dics[p_id]['resize'])
-        h=int(dics[p_id]['resize'])
-        imageName=dics[p_id]['name']
-        # //mi cek sa her don mi zvoglu foton
+        width=int(resize)
+        h=int(resize)
+        imageName=name
+      
         img=image.resize((int(image.width/width),int(image.height/h)))
-        img.save(f"{path}{imageName}.jpg")
+        nameR=resizeName
+        
+        img.save(f"{path}{imageName}{nameR}.jpg")
         print(img.width,img.height)
         img.show()
 
-def thumb(dics, path):
-    for p_id, p_info in dics.items():
-        im = Image.open(dics[p_id]['url'])
-        im.thumbnail(dics[p_id]['resize'])
-        im.save(dics[p_id]['name'], ".JPEG")
+def thumb(name,resize,resizeName, path):
+
+        im = Image.open(path+name+".jpg")
+        im.show()
+        width=int(resize)
+        # print(width)
+        # h=int(resize)
+        # print(h)
+        imageName=resizeName
+        print(imageName)
+        # //mi cek sa her don mi zvoglu foton
+        img=im.resize((int(im.width/width),int(im.height/h)))
+        img.save(f"{path}{imageName}.jpg")
 
 
 class ImageController:
@@ -107,18 +90,30 @@ class ImageController:
         task_index: int = 0
         for task in range(task_number):
             while True:
-                images_link= str(input(f"Shkruaj linkun e fotos: "))
-                iamges_name= str(input(f"Shkruaj emrin e fotos se si deshironi ta ruani: "))
-                if(action==2 or action==3):
-                    resize_number= int(input(f"Shkruaj sa here deshironi ta zvogeloni foton: "))
-                else:
+                if action==1:
+                    images_link= str(input(f"Shkruaj linkun e fotos: "))
+                    iamges_name= str(input(f"Shkruaj emrin e fotos se si deshironi ta ruani: "))
                     resize_number=0
+                    resize_name=NULL
+                elif( action==3):
+                    images_link= str(input(f"Shkruaj linkun e fotos: "))
+                    iamges_name= str(input(f"Shkruaj emrin e fotos se si deshironi ta ruani: "))
+                    resize_number= int(input(f"Shkruaj sa here deshironi ta zvogeloni foton: "))
+                    resize_name=input(f"shrkuaj emrin e fotos se zvogeluar")
+                elif(action==2):
+                    images_link=NULL
+                    iamges_name= str(input(f"Shkruaj emrin e fotos qe deshironi ta zvogeloni: "))
+                    resize_number= int(input(f"Shkruaj sa here deshironi ta zvogeloni foton: "))
+                    resize_name=input(f"shrkuaj emrin e fotos se zvogeluar")
+               
+                    
                 
-                if images_link and iamges_name:
+                if iamges_name:
                     dics[task_index] = {
                         "url": images_link,
                         "name": iamges_name,
                         "resize": resize_number,
+                        "resizeName":resize_name,
                     }
                     task_index = task_index + 1
                     break
@@ -135,16 +130,26 @@ class ImageController:
                 print(i)
                 print("MULTIPROCESING PROCESSES")
                 tic=time.time()
-                p=multiprocessing.Process(target=download_multi, args=(dics,path))
+                p=multiprocessing.Process(target=download_multi, args=(dics[i]['url'], dics[i]['name'],path))
+            
                 processes.append(p)
                 toc=time.time()
-                # print("Koha: " str(toc-tic))
+                
             elif action == 2:
-                thumb(dics,path)
-            elif action == 3:      
-                p=multiprocessing.Process(target=thumbnail, args=(dics,path))
+              
+                tic=time.time()
+                p=multiprocessing.Process(target=thumb, args=(dics[i]['name'],dics[i]['resize'],dics[i]['resizeName'],path))
                 processes.append(p)
+                toc=time.time()
+                
+            elif action == 3:  
+                tic=time.time()    
+                p=multiprocessing.Process(target=thumbnail, args=(dics[i]['url'], dics[i]['name'],dics[i]['resize'],dics[i]['resizeName'],path))
+                processes.append(p)
+                toc=time.time()
+                
             print("Shkarkimi filloi për linkun ", i + 1, " koha: ", ctime(), )
+            # print(toc-tic)
             processes[i].start()   
         for i in range(0, task_number):
             processes[i].join()
